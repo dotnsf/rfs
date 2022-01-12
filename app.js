9b2281cbe2981da3c68f6cfa5567afe99c6cb3d6
K.Kimura <dotnsf@gmail.com>
2022-01-12 22:03:10 +0900

    #1 fixed.

--- app.js
+++ app.js
@@ -272,6 +272,13 @@ app.all( '/*', async function( req, res, next ){
             res.write( JSON.stringify( r, null, 2 ) );
             res.end();
           }else{
+            if( req.file && req.file.path ){
+              //. ファイル作成
+              record.contenttype = req.file.mimetype;
+              var filepath = req.file.path;
+              record.body = fs.readFileSync( filepath );
+            }
+
             //. ファイル更新
             var r = await editfile( path, record, owner_id );
             if( !r.status ){
